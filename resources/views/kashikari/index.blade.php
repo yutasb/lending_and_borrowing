231c0ca0ae2c5326cffe89a06c391c0ae6770d11
yutasb <shibamoto.yuta@gmail.com>
2020-01-14 22:30:52 +0900

    grid ok

--- resources/views/kashikari/index.blade.php
+++ resources/views/kashikari/index.blade.php
@@ -2,45 +2,38 @@
 
 @section('content')
 
-
-<div class="col-sm-2">
-
-    <form method='get' action="{{ route('kashikari.wordsearch','title') }}">
-        <input type='text' name='title'><input type='submit' value='検索'>
-    </form><br><br>
-
-    <a href="/lent"> <input type=button class='btn btn-outline-primary w-75' value='すべて'></a><br><br>
-    @foreach(config('category') as $category=>$name )
-    <a href="{{route('kashikari.search',$category)}}"><input value="{{ $name }}" type='button' class='btn btn-outline-primary w-75'> </a> <br><br>
-    @endforeach
-
-</div>
-
-
-<div class="container">
-    <h2>{{__('Lent List')}}</h2>
-
+<div class='container'>
     <div class="row">
-        @foreach($kashikaris as $kashikari)
         <div class="col-sm-3">
-            <div class="card">
-                <div class="card-body  text-center">
-                    <a href="{{ route('kashikari.show',$kashikari->id ) }}">
-                        <img src="{{asset('storage/post_images/'.$kashikari->pic1)}}" alt='イメージ画像' width=150px>
-                        <h5 class="card-title">{{$kashikari->title}}</h5>
-                        <!-- いいね -->
-                        <!-- <a href='/lent/{id}/like' >
-                            <button type="submit">
-                                <i class="fas fa-heart"></i>
-                            </button>
 
-                            </a>-->
-                    </a>
-                </div>
+            <form method='get' action="{{ route('kashikari.wordsearch','title') }}">
+                <input type='text' name='title'><input type='submit' value='検索'>
+            </form><br><br>
+
+            <a href="/lent"> <input type=button class='btn btn-outline-primary w-75' value='すべて'></a><br><br>
+            @foreach(config('category') as $category=>$name )
+            <a href="{{route('kashikari.search',$category)}}"><input value="{{ $name }}" type='button' class='btn btn-outline-primary w-75'> </a> <br><br>
+            @endforeach
+
+        </div>
+
+
+
+        <div class="col-sm-9">
+            <h2>{{__('Lent List')}}</h2><br>
+            <div class="row">
+
+                @foreach($kashikaris as $kashikari)
+                <a href="{{ route('kashikari.show',$kashikari->id ) }}">
+                    <img src="{{asset('storage/post_images/'.$kashikari->pic1)}}" alt='イメージ画像' width=180px class='ml-3'> 　
+                    <h5 class="card-title text-center">
+                        {{$kashikari->title}}　
+                    </h5>
+                </a>
+                @endforeach
+
             </div>
         </div>
-        @endforeach
     </div>
-
 </div>
 @endsection
