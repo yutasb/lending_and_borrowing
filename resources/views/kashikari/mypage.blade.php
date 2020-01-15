8454c13cdda211761c94a0758c46b919acdb9a89
yutasb <shibamoto.yuta@gmail.com>
2020-01-15 23:37:06 +0900

    using page show

--- resources/views/kashikari/mypage.blade.php
+++ resources/views/kashikari/mypage.blade.php
@@ -30,28 +30,27 @@
     <br><br><br>
 
     <h2>{{__('Using')}}</h2>
-    @foreach($kashikaris as $kashikari)
-    @if($kashikari->kashikari_using == 1)
-    <div class="col-sm-4">
-        <div class="card text-danger ml-3 mb-3">
-            <div class="card-body text-center">
-                <a href="{{ route('kashikari.show',$kashikari->id ) }}">
-                    <img src="{{asset('storage/post_images/'.$kashikari->pic1)}}" alt='イメージ画像' width=180px>
-                    <h5 class="card-title text-center">
-                        {{$kashikari->title}}　
-                    </h5>
-                    <div class="card-img-overlay">　
-                        <h2 class='card-text'>{{__('Using')}}</h2>
-                    </div>
-                </a>
+    <div class="row">
+        @foreach($kashikaris as $kashikari)
+        @if($kashikari->kashikari_using == 1)
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body text-center">
+                    <a href="{{ route('kashikari.show',$kashikari->id ) }}">
+                        <img src="{{asset('storage/post_images/'.$kashikari->pic1)}}" alt='イメージ画像' width=180px>
+                        <h5 class="card-title text-center">
+                            {{$kashikari->title}}　
+                        </h5>
+                        <div class="card-img-overlay">　
+                            <h2 class='card-text'>{{__('Using')}}</h2>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
 
+        @endif
+        @endforeach
     </div>
-    @endif
-    @endforeach
-
-
-
 </div>
 @endsection
--- resources/views/kashikari/otherprofile.blade.php
+++ resources/views/kashikari/otherprofile.blade.php
@@ -14,6 +14,7 @@
     <h2>{{__('My Lent List')}}</h2>
     <div class='row'>
         @foreach($kashikaris as $kashikari)
+        @if($kashikari->kashikari_using == 0)
         <div class="col-sm-4">
             <div class="card">
                 <div class='card-body  text-center'>
@@ -24,6 +25,32 @@
                 </div>
             </div>
         </div>
+        @endif
+        @endforeach
+    </div>
+    <br><br><br>
+
+
+    <h2>{{__('Using')}}</h2>
+    <div class='row'>
+        @foreach($kashikaris as $kashikari)
+        @if($kashikari->kashikari_using == 1)
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body text-center">
+                    <a href="{{ route('kashikari.show',$kashikari->id ) }}">
+                        <img src="{{asset('storage/post_images/'.$kashikari->pic1)}}" alt='イメージ画像' width=180px>
+                        <h5 class="card-title text-center">
+                            {{$kashikari->title}}　
+                        </h5>
+                        <div class="card-img-overlay">　
+                            <h2 class='card-text'>{{__('Using')}}</h2>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        @endif
         @endforeach
     </div>
 </div>
