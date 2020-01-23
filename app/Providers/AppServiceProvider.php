9249914d25d0141bd68eb8e6ca45f3c416d6409d
yutasb <shibamoto.yuta@gmail.com>
2020-01-24 00:49:53 +0900

    varchar ok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        Schema::defaultStringLength(191);
     }
 }
