449f32f2287b6c8bc7c0994355e31d5fc70a6df3
yutasb <shibamoto.yuta@gmail.com>
2020-01-24 12:16:12 +0900

    unix delete

--- config/database.php
+++ config/database.php
@@ -57,7 +57,6 @@ return [
             'database' => env('DB_DATABASE', 'kashikari'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
-            'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
