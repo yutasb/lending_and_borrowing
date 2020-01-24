607bc17ba36d8657c448bc220fc1cfac104edc99
yutasb <shibamoto.yuta@gmail.com>
2020-01-25 00:38:59 +0900

    show revise

--- resources/views/kashikari/show.blade.php
+++ resources/views/kashikari/show.blade.php
@@ -4,6 +4,8 @@
 <div class="container">
     <div class='row justify-content-center'>
         <div class="col-md-9">
+            <p class='text-right'><a href="{{route('kashikari')}}">商品一覧へ>></a></p>
+
             <div class="card">
                 <div class="card-header">
 
